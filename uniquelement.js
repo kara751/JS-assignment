4bbe877db163134f9c1213556684ffc5d6b42677
kara751 <64679205+kara751@users.noreply.github.com>
2021-05-26 18:47:42 +0530

    Update uniquelement.js

--- uniquelement.js
+++ uniquelement.js
@@ -1,6 +1,2 @@
-var arr1=[1,2,3];
-var arr2=[100,2,1,10];
-var a=arr1.filter((o => arr2.indexOf(o)=== -1));
-var b=arr2.filter((o => arr1.indexOf(o)=== -1));
-var result=a.concat(b);
+var result=_.union([1,2,3],[100,2,1,10])
 document.write("<h1>"+result+"</h1>");
